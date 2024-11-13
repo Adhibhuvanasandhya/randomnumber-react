0d66c1c37f33a2e6a6c61b5837f6846ff7299cf7
Adhibhuvanasandhya <sandhyaponrajan.l@gmail.com>
2024-11-13 09:29:58 +0530

    updated project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import"./style.css"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [minVal, setMinVal] = useState(0);
+    const [maxVal, setMaxVal] = useState(10);
+    const [randomNum, setRandomNum] = useState(0);
 
-export default App;
+    const [history, setHistory] = useState([]);
+
+    const handleRandomNum = () => {
+        const newRandomNum = Math.floor(Math.random() * (maxVal - minVal + 1) + minVal);
+        setRandomNum(newRandomNum);
+        setHistory((prevHistory) => [...prevHistory, newRandomNum]);   
+    }
+     const change = ()=>{
+        setRandomNum(null)
+        setHistory([])
+     }
+    return (
+        <div className="hero">
+            <div className="container">
+                <div className="randomNum">
+                    <p>
+                        Random Numer: <span>{randomNum}</span>
+                    </p>
+                
+                </div>
+                <div className="numContainer">
+                    <div>
+                        <p>Min</p>
+                        <input
+                            type="number"
+                            value={minVal}
+                            onChange={(evt) => setMinVal(evt.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <p>Max</p>
+                        <input
+                            type="number"
+                            value={maxVal}
+                            onChange={(evt) => setMaxVal(evt.target.value)}
+                        />
+                    </div>
+                </div>
+                <button onClick={handleRandomNum}>Get Random Numer</button>
+
+                
+                
+                 <div className="history">
+                    <h3 style={{color:"#9cff9c"}}>History:</h3>
+                    <ul>
+                        {history.map((num, index) => (
+                            <li key={index}>{num}</li>
+                        ))}
+                    </ul>
+                    
+                </div>
+                <button onClick={change}>Reset</button>
+
+            </div>
+        </div>
+    )
+
+}
+export default App
